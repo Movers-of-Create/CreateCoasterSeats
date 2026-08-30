1d0fd3f338d37578fbdf31f59dea69d3f84af725
Villagerzock <126190361+Villagerzock@users.noreply.github.com>
2026-08-30 11:27:53 +0200

    Bug Fix: #4 First Person Render Bug

--- src/main/java/net/villagerzock/createcoasterseats/mixin/PlayerModelMixin.java
+++ src/main/java/net/villagerzock/createcoasterseats/mixin/PlayerModelMixin.java
@@ -13,13 +13,35 @@ import net.neoforged.fml.common.Mod;
 import net.villagerzock.createcoasterseats.block.PlayerModelBundle;
 import net.villagerzock.createcoasterseats.block.SecurableSeatBlock;
 import net.villagerzock.createcoasterseats.block.entity.SecurableSeatBlockEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerModel.class)
 public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
+    @Shadow
+    @Final
+    public ModelPart jacket;
+
+    @Shadow
+    @Final
+    public ModelPart leftPants;
+
+    @Shadow
+    @Final
+    public ModelPart rightPants;
+
+    @Shadow
+    @Final
+    public ModelPart leftSleeve;
+
+    @Shadow
+    @Final
+    public ModelPart rightSleeve;
+
     public PlayerModelMixin(ModelPart root) {
         super(root);
     }
@@ -31,8 +53,14 @@ public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
         }
         float partialTicks = Minecraft.getInstance().getTimer().getGameTimeDeltaPartialTick(true);
         if (entity.getVehicle() instanceof SeatEntity seat && seat.level().getBlockState(seat.blockPosition()).getBlock() instanceof SecurableSeatBlock securableSeatBlock && seat.level().getBlockEntity(seat.blockPosition()) instanceof SecurableSeatBlockEntity securableSeatBlockEntity && securableSeatBlockEntity.isHangerDown(partialTicks)){
-            securableSeatBlock.updatePlayerAnimation(player, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, partialTicks, new PlayerModelBundle(this.head, this.body, this.rightArm, this.leftArm, this.rightLeg, this.leftLeg), seat.blockPosition(), seat.level(),Minecraft.getInstance().options.getCameraType().isFirstPerson());
-            if (Minecraft.getInstance().options.getCameraType().isFirstPerson()){
+            boolean firstPerson = entity == Minecraft.getInstance().cameraEntity && Minecraft.getInstance().options.getCameraType().isFirstPerson();
+            securableSeatBlock.updatePlayerAnimation(player, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, partialTicks, new PlayerModelBundle(this.head, this.body, this.rightArm, this.leftArm, this.rightLeg, this.leftLeg), seat.blockPosition(), seat.level(), firstPerson);
+            this.jacket.visible = this.body.visible;
+            this.leftPants.visible = this.leftLeg.visible;
+            this.rightPants.visible = this.rightLeg.visible;
+            this.leftSleeve.visible = this.leftArm.visible;
+            this.rightSleeve.visible = this.rightArm.visible;
+            if (firstPerson){
                 this.head.visible = false;
                 this.hat.visible = false;
             }
